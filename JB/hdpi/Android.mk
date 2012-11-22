9a708f6665896ef41d1c1185ed826c9a672c35fe
Paul Beeler <sparksco@gmail.com>
2012-11-22 06:21:21 -0700

    Use a Android.mk file to build prebuilt

---
+++ JB/hdpi/Android.mk
@@ -0,0 +1,9 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := Wallpaperz.apk
+LOCAL_SRC_FILES := Wallpaperz.apk
+LOCAL_MODULE_CLASS := APPS
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_MODULE_PATH := $(TARGET_OUT)/app
+include $(BUILD_PREBUILT)
