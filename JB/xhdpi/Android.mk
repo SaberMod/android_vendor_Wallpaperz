dc946cfdc5e0c826453a67e5db068ae89da4c3a1
Paul Beeler <sparksco@gmail.com>
2013-01-31 22:41:04 -0700

    Add xhdpi makefile

---
+++ JB/xhdpi/Android.mk
@@ -0,0 +1,11 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := Wallpaperz-xhdpi
+LOCAL_SRC_FILES := Wallpaperz.apk
+LOCAL_MODULE_SUFFIX := .apk
+LOCAL_MODULE_CLASS := APPS
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_MODULE_PATH := $(TARGET_OUT)/app
+include $(BUILD_PREBUILT)
+
